a374c74975d1751e13932cced6c26a330a9bf194
Himanshubhadoria11 <himanshubhadoria22@gmail.com>
2024-08-01 20:02:06 +0530

    a

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
-// const live_url='mongodb+srv://himanshu:Hsb81996@cluster0.klwkh7z.mongodb.net/Admission_Portal?retryWrites=true&w=majority&appName=Cluster0'
-const local_URL = 'mongodb://127.0.0.1:27017/Admission_portal'
+const live_url='mongodb+srv://himanshu:Hsb81996@cluster0.klwkh7z.mongodb.net/Admission_Portal?retryWrites=true&w=majority&appName=Cluster0'
+const local_url = 'mongodb://127.0.0.1:27017/Admission_portal'
 
 const connectDb = () => {
     // return mongoose.connect(local_URL)
-    return mongoose.connect(local_URL)
+    return mongoose.connect(live_url)
         .then(() => {
             console.log('connect Success')
         }).catch((error) => {
